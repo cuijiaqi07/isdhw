91db4a56049ef688e43bfa9b67bb22e5371fd765
cuijiaqi07 <244783062@qq.com>
2015-04-16 22:03:34 +0800

    homework1.

---
+++ 01/main.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+        for j in range(i,10):
+            m=str.format("{0:1}*{1:1}={2:>2}",i,j,i*j)
+            print(m,end="  ")
+        print()
+for i in range(1,10):
+        for k in range(0,i-1):
+            print(end="        ")
+        for j in range(i,10):
+            n=str.format("{0:1}*{1:1}={2:>2}",i,j,i*j) 
+            print(n,end="  ")
+        print()
+
